7be427bcc8d2db73e30ffc513b0cee4a1facdbcf
Ankush Singh <146627011+Ankushrajput2006@users.noreply.github.com>
2026-07-21 23:08:51 +0530

    Time: 1 ms (99.96%), Space: 47 MB (46.16%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,10 @@
 import java.util.HashMap;
 class Solution {
     public int singleNumber(int[] nums) {
-       HashMap<Integer, Integer> countMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-        }
-        for (int num : countMap.keySet()) {
-            if (countMap.get(num) == 1) {
-                return num;
-            }
-        }
-        return -1; // Return -1 if no unique number is found
+       int r = 0;
+       for(int i=0;i<nums.length;i++){
+         r = r^nums[i];
+       }
+       return r;
     }
 }
